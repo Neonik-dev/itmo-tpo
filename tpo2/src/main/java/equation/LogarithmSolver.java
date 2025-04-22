387526c6d3e6ac5af7672b784fbb6fe74fd02016
neonik <lis.gleb2012@gmail.com>
2025-04-22 21:00:40 +0300

    52-2

--- tpo2/src/main/java/equation/LogarithmSolver.java
+++ tpo2/src/main/java/equation/LogarithmSolver.java
@@ -1,8 +1,17 @@
 package equation;
 
-public class LogarithmSolver implements Solver{
+import logarithm.Log3;
+
+public class LogarithmSolver implements Solver {
+
+    private final Log3 log3;
+
+    public LogarithmSolver(Log3 log3) {
+        this.log3 = log3;
+    }
+
     @Override
     public double compute(double x, double eps) {
-        return 0;
+        return Math.pow(Math.pow(Math.pow(Math.pow(Math.pow(log3.compute(x, eps), 2), 3), 2), 3), 3);
     }
 }
--- tpo2/src/main/java/logarithm/Log3.java
+++ tpo2/src/main/java/logarithm/Log3.java
@@ -1,7 +1,11 @@
 package logarithm;
 
 public class Log3 {
-    private final Ln ln = new Ln();
+    private final Ln ln;
+
+    public Log3(Ln ln) {
+        this.ln = ln;
+    }
 
     public double compute(double x, double eps) {
         return ln.compute(x, eps) / 1.09861228867;
