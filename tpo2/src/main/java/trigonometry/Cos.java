e920b2d9937bc444439c72c058bd834a6a0a3401
martin <martin.valtz@gmail.com>
2025-04-29 16:42:39 +0300

    lab2

---
+++ tpo2/src/main/java/trigonometry/Cos.java
@@ -0,0 +1,43 @@
+package trigonometry;
+
+public class Cos {
+
+    private static final double PI = Math.PI;
+    private static final double TWO_PI = 2 * PI;
+    private final Sin sin;
+
+    public Cos() {
+        this.sin = new Sin();
+    }
+
+    public double compute(double x, double eps) {
+        if (!validX(x)) {
+            throw new ArithmeticException("Wrong x");
+        }
+
+        x %= TWO_PI;
+        if (x <= -PI) {
+            x += TWO_PI;
+        } else if (x > PI) {
+            x -= TWO_PI;
+        }
+
+        if (x == PI / 2 || x == -PI / 2) {
+            return 0d;
+        }
+
+        double sinX = sin.compute(x, eps);
+        double res = Math.sqrt(1 - Math.pow(sinX, 2));
+
+        boolean isNegative = (x > PI / 2 && x <= PI) || (x < -PI / 2 && x >= -PI);
+
+        return isNegative ? -res : res;
+    }
+
+    public boolean validX(double x) {
+        if (!(Double.isNaN(x) || Double.isInfinite(x))) {
+            return false;
+        }
+        return x <= 0;
+    }
+}
---
+++ tpo2/src/main/java/trigonometry/Sin.java
@@ -0,0 +1,46 @@
+package trigonometry;
+
+public class Sin {
+
+    private static final double PI = Math.PI;
+
+    public double compute(double x, double eps) {
+        if (!validX(x)) {
+            throw new ArithmeticException("Неверный аргумент x");
+        }
+
+        x %= 2 * PI;
+
+        if (x == 0 || x == PI) {
+            return 0d;
+        }
+
+        double res = x;
+        int k = 3;
+        double pow = Math.pow(x, 3);
+        int sign = -1;
+        long fact = 6; // 3! = 6
+
+        while (true) {
+            double curTerm = (sign * pow) / fact;
+            if (Math.abs(curTerm) < eps) {
+                break;
+            }
+
+            res += curTerm;
+            sign = -sign;
+            fact *= (long) (k + 1) * (k + 2); // (k+1)*(k+2) на следующую итерацию
+            pow *= Math.pow(x, 2); // x^(k+2)
+            k += 2;
+        }
+
+        return res;
+    }
+
+    public boolean validX(double x) {
+        if (!(Double.isNaN(x) || Double.isInfinite(x))) {
+            return false;
+        }
+        return x <= 0;
+    }
+}
