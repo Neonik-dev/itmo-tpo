b25d82c7ae6648b435ecf70f35c94f188b89469d
neonik <lis.gleb2012@gmail.com>
2025-04-29 18:19:03 +0300

    52-4

--- tpo2/src/test/java/equation/logarithm/LnTest.java
+++ tpo2/src/test/java/equation/logarithm/LnTest.java
@@ -17,7 +17,7 @@ class LnTest {
     @Test
     void checkValidationX_ThrowIllegalArgumentException() {
         assertAll(
-                () -> assertThrows(IllegalArgumentException.class, () -> ln.compute(-1, 0.001)),
+                () -> assertThrows(IllegalArgumentException.class, () -> ln.compute(-2, 0.001)),
                 () -> assertThrows(IllegalArgumentException.class, () -> ln.compute(0, 0.001)),
                 () -> assertThrows(IllegalArgumentException.class, () -> ln.compute(Double.NaN, 0.001)),
                 () -> assertThrows(IllegalArgumentException.class, () -> ln.compute(Double.NEGATIVE_INFINITY, 0.001)),
@@ -27,10 +27,23 @@ class LnTest {
 
     @Test
     void checkLn_OK() {
+        double delta = 0.01;
+        double eps = 0.001;
         assertAll(
-                () -> assertEquals(0, ln.compute(1, 0.001)),
-                () -> assertEquals(2.302585, ln.compute(10, 0.001), 0.01),
-                () -> assertEquals(2.70805, ln.compute(15, 0.001), 0.01)
+                () -> assertEquals(0, ln.compute(1, eps)),
+                () -> assertEquals(2.302585, ln.compute(10, eps), delta),
+                () -> assertEquals(2.70805, ln.compute(15, eps), delta),
+                () -> assertEquals(1, ln.compute(Math.E, eps), delta)
+        );
+    }
+
+    @Test
+    void checkLargeLn_OK() {
+        double eps = 0.001;
+        assertAll(
+                () -> assertEquals(8.38213, ln.compute(12342, eps), eps),
+                () -> assertEquals(8.87126, ln.compute(34234893284.23423, eps), eps),
+                () -> assertEquals(7.86001, ln.compute(4343, eps), eps)
         );
     }
 }
--- tpo2/src/test/java/equation/logarithm/Log3Test.java
+++ tpo2/src/test/java/equation/logarithm/Log3Test.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class Log3Test {
+class Log3Test extends LnTest {
     private static Log3 log3;
 
     @BeforeAll
@@ -16,22 +16,21 @@ class Log3Test {
     }
 
     @Test
-    void checkValidationX_ThrowIllegalArgumentException() {
-        assertAll(
-                () -> assertThrows(IllegalArgumentException.class, () -> log3.compute(-1, 0.001)),
-                () -> assertThrows(IllegalArgumentException.class, () -> log3.compute(0, 0.001)),
-                () -> assertThrows(IllegalArgumentException.class, () -> log3.compute(Double.NaN, 0.001)),
-                () -> assertThrows(IllegalArgumentException.class, () -> log3.compute(Double.NEGATIVE_INFINITY, 0.001)),
-                () -> assertThrows(IllegalArgumentException.class, () -> log3.compute(Double.POSITIVE_INFINITY, 0.001))
-        );
-    }
-
-    @Test
     void checkLn_OK() {
         assertAll(
                 () -> assertEquals(0, log3.compute(1, 0.001)),
                 () -> assertEquals(2.0959, log3.compute(10, 0.001), 0.01),
                 () -> assertEquals(2.4649, log3.compute(15, 0.001), 0.01)
+        );
+    }
+
+    @Test
+    void checkLargeLn_OK() {
+        double eps = 0.001;
+        assertAll(
+                () -> assertEquals(7.62974, log3.compute(12342, eps), eps),
+                () -> assertEquals(8.07497, log3.compute(34234893284.23423, eps), eps),
+                () -> assertEquals(7.15449, log3.compute(4343, eps), eps)
         );
     }
 }
