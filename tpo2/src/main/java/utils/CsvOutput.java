607230f3ba202fa49eda9a917931041f64e7ebfc
neonik <lis.gleb2012@gmail.com>
2025-04-29 18:29:57 +0300

    47

---
+++ tpo2/src/main/java/utils/CsvOutput.java
@@ -0,0 +1,29 @@
+package utils;
+
+import lombok.Data;
+import lombok.SneakyThrows;
+
+import java.io.*;
+
+@Data
+public class CsvOutput {
+    private String filePath;
+
+    public void setFilePath(String fileName) {
+        this.filePath = fileName;
+    }
+
+    @SneakyThrows
+    public void clearFile() {
+        try (PrintWriter writer = new PrintWriter(filePath)) {
+            writer.print("");
+        }
+    }
+
+    @SneakyThrows
+    public void logging(double x, double y) {
+        try (PrintStream printStream = new PrintStream(new FileOutputStream(filePath, true))) {
+            printStream.printf("%s, %s\n", x, y);
+        }
+    }
+}
