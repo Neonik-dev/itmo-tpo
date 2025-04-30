027ac2b1f16c2a98975b08746507e821c27a827c
martin <martin.valtz@gmail.com>
2025-04-30 19:36:15 +0300

    lab52-49

--- tpo2/src/test/java/equation/TrigonometrySolverTest.java
+++ tpo2/src/test/java/equation/TrigonometrySolverTest.java
@@ -11,12 +11,14 @@ import org.junit.jupiter.params.provider.CsvFileSource;
 import org.mockito.Mockito;
 import trigonometry.Cos;
 import trigonometry.Sin;
+import utils.CsvOutput;
 
 import static org.junit.Assert.assertEquals;
 
 public class TrigonometrySolverTest {
     public static Sin sin = Mockito.mock(Sin.class);
     public static Cos cos = Mockito.mock(Cos.class);
+    private static final CsvOutput csvOutput = new CsvOutput();
 
     @BeforeAll
     public static void fillAll() {
@@ -62,6 +64,7 @@ public class TrigonometrySolverTest {
         double x = divisible * Math.PI / divider, result;
         try {
             result = ts.compute(x, 0.001);
+            csvOutput.logging(x, result);
             assertEquals(trueResult, result, 0.001);
         } catch (ArithmeticException e) {
             assertEquals("Wrong x", e.getMessage());
@@ -71,6 +74,7 @@ public class TrigonometrySolverTest {
     @ParameterizedTest
     @CsvFileSource(resources = "/trigonometryTestData/test-trig-func-data.csv")
     void isolatedFunctionTest(Double divisible, Double divider, Double trueResult) {
+        csvOutput.setFilePath("src/test/resources/result/equation/trigonometrySolverAnswer.csv");
         TrigonometricSolver trigonometricExpression
                 = new TrigonometricSolver(cos);
         runTest(trigonometricExpression, divisible, divider, trueResult);
@@ -79,6 +83,7 @@ public class TrigonometrySolverTest {
     @ParameterizedTest
     @CsvFileSource(resources = "/trigonometryTestData/test-trig-func-data.csv")
     void cosTest(Double divisible, Double divider, Double trueResult) {
+        csvOutput.setFilePath("src/test/resources/result/equation/trigonometrySolverAnswer.csv");
         TrigonometricSolver trigonometricExpression
                 = new TrigonometricSolver(new Cos(sin));
         runTest(trigonometricExpression, divisible, divider, trueResult);
@@ -87,6 +92,7 @@ public class TrigonometrySolverTest {
     @ParameterizedTest
     @CsvFileSource(resources = "/trigonometryTestData/test-trig-func-data.csv")
     void fullTest(Double divisible, Double divider, Double trueResult) {
+        csvOutput.setFilePath("src/test/resources/result/equation/trigonometrySolverAnswer.csv");
         TrigonometricSolver trigonometricExpression
                 = new TrigonometricSolver(new Cos(new Sin()));
         runTest(trigonometricExpression, divisible, divider, trueResult);
